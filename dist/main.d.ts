69c54a5b5bdd4806d24a06fdfa1db8ca5be24634
hugoalh <32359235+hugoalh@users.noreply.github.com>
2024-02-02 17:04:44 +0800

    Update workflow

--- dist/main.d.ts
+++
@@ -1,4 +0,0 @@
-export { default, ExFetch, exFetch, exFetchPaginate, httpStatusCodesRetryable, userAgentDefault, type ExFetchDelayOptions, type ExFetchEventPaginatePayload, type ExFetchEventRedirectPayload, type ExFetchEventRetryPayload, type ExFetchOptions, type ExFetchPaginateLinkUpPayload, type ExFetchPaginateOptions, type ExFetchRedirectOptions, type ExFetchRetryOptions } from "./exfetch.js";
-export { HTTPHeaderLink, type HTTPHeaderLinkEntry } from "./header/link.js";
-export { HTTPHeaderRetryAfter } from "./header/retry-after.js";
-//# sourceMappingURL=main.d.ts.map
--- dist/main.js
+++
@@ -1,3 +0,0 @@
-export { default, ExFetch, exFetch, exFetchPaginate, httpStatusCodesRetryable, userAgentDefault } from "./exfetch.js";
-export { HTTPHeaderLink } from "./header/link.js";
-export { HTTPHeaderRetryAfter } from "./header/retry-after.js";
